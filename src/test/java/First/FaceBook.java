eaa1b08bb984fb6450fa88d6a7365f1e2df48d59
avish <avishtest@gmail.com>
2022-11-27 11:20:07 +0530

    new project

---
+++ src/test/java/First/FaceBook.java
@@ -0,0 +1,25 @@
+package First;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FaceBook {
+
+	
+	
+	@Test
+	public void data() 
+	{
+		Assert.assertTrue(true);
+		
+		
+	}
+	
+	@Test
+	public void Metadata() 
+	{
+		Assert.assertTrue(true);
+		
+		
+	}
+}
